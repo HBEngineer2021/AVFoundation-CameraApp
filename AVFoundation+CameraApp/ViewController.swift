ea3c0a49ba41dcfebdf1f5c777d3d9c1051ab330
Hanagasaki <stze961q@gmail.com>
2021-12-26 02:38:31 +0900

    カメラ起動、停止機能を実装。

--- AVFoundation+CameraApp/ViewController.swift
+++ AVFoundation+CameraApp/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var previewImage: UIImageView!
+    
+    @IBOutlet weak var snapshotBtn: UIButton! {
+        didSet {
+            snapshotBtn.backgroundColor = UIColor.white
+            snapshotBtn.layer.cornerRadius = snapshotBtn.frame.size.width/2
+        }
+    }
+    
+    let videoCapture = VideoCapture()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        start()
     }
-
-
+    
+    @IBAction func stopBtn(_ sender: Any) {
+        stop()
+    }
+    
+    func start() {
+        videoCapture.run { CMSampleBuffer in
+            if let convertImage = self.UIImageFromSampleBuffer(CMSampleBuffer) {
+                DispatchQueue.main.async {
+                    self.previewImage.image = convertImage
+                }
+            }
+        }
+    }
+    
+    func stop() {
+        videoCapture.stop()
+    }
+    
+    func UIImageFromSampleBuffer(_ sampleBuffer: CMSampleBuffer) -> UIImage? {
+        if let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
+            let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
+            let context = CIContext()
+            if let image = context.createCGImage(ciImage, from: imageRect) {
+                return UIImage(cgImage: image)
+            }
+        }
+        return nil
+    }
+    
+    
 }
 
